834a88fecea99efc5a294620119dbe20c1beb99f
aleekwa <leekwa@gmail.com>
2022-05-14 22:18:19 +0800

    Created search function (by restaurant name and category) via Query String; optimized UX: 1) input case irrelevant, 2) keep search keyword in the search box after showing search result

--- app.js
+++ app.js
@@ -13,15 +13,31 @@ app.set("view engine", "handlebars");
 app.use(express.static("public"));
 
 // routes setting
+
+//route for index page
 app.get("/", (req, res) => {
   res.render("index", { restaurants: restaurantList.results });
 });
 
+//dynamic routing for individual restaurant details
 app.get("/restaurants/:restaurant_id", (req, res) => {
-  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  res.render('show', { restaurant: restaurant })
-})
-  ;
+  const restaurant = restaurantList.results.find(
+    (restaurant) => restaurant.id.toString() === req.params.restaurant_id
+  );
+  res.render("show", { restaurant: restaurant });
+});
+
+//dynamic routing for search function - by name or category
+app.get("/search", (req, res) => {
+  const keyword = req.query.keyword;
+  const restaurants = restaurantList.results.filter((restaurant) => {
+    return (
+      restaurant.name.toLowerCase().includes(keyword) ||
+      restaurant.category.toLowerCase().includes(keyword)
+    );
+  });
+  res.render("index", { restaurants: restaurants, keyword: keyword });
+});
 
 // start and listen on the Express server
 app.listen(port, () => {
