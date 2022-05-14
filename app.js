9bc850b4d667461c303c754c39df32229b313be8
aleekwa <leekwa@gmail.com>
2022-05-14 22:01:00 +0800

    created partial template (show.handlebars) for showing individual restaurant details upon click; enabled dynamic routing

--- app.js
+++ app.js
@@ -14,8 +14,14 @@ app.use(express.static("public"));
 
 // routes setting
 app.get("/", (req, res) => {
-  res.render("index", {restaurants:restaurantList.results});
+  res.render("index", { restaurants: restaurantList.results });
 });
+
+app.get("/restaurants/:restaurant_id", (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant })
+})
+  ;
 
 // start and listen on the Express server
 app.listen(port, () => {
